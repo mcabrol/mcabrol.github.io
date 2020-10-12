35eb95c77cbc66d8b06dec0bcd33b572edeabf27
Bob <Bob@Bobs-iMac.local>
2020-10-12 20:25:33 +0200

    Update index.html, add hover function and a clock.

--- profile.js
+++ profile.js
@@ -1,8 +1,50 @@
-var request = $.ajax({
-	method: "GET",
-	url: "https://mcabrol.github.io/README.md"
-});
+function currentTime() {
+	var date = new Date();
+	var hour = date.getHours();
+	var min = date.getMinutes();
+	var sec = date.getSeconds();
+	hour = updateTime(hour);
+	min = updateTime(min);
+	sec = updateTime(sec);
+	document.getElementById("clock").innerText = hour + " : " + min + " : " + sec;
+	var t = setTimeout(function(){ currentTime() }, 1000);
+}
 
-request.done(function(data) {
-	$('#content').append(marked(data));
+function updateTime(k) {
+	if (k < 10) {
+		return ("0" + k);
+	}
+	else {
+		return (k);
+	}
+}
+
+function sendEmail()
+{
+    window.location = "mailto:martin.cabrol@free.fr";
+}
+
+$( document ).ready(function() {
+
+	var request = $.ajax({
+		method: "GET",
+		url: "https://mcabrol.github.io/README.md"
+	});
+
+	request.done(function(data) {
+		$('#content').append(marked(data));
+	});
+
+	$('#key').hover(
+		  function () {
+			  $("#main").css({"color":"transparent"});
+			  $("#contact").css({"color":"white"});
+		  },
+		  function () {
+			  $("#contact").css({"color":"transparent"});
+			  $("#main").css({"color":"white"});
+		  }
+	  );
+
+	  currentTime();
 });
