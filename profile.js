acbb8d5bfb0ba0571b79a5759773ba036db378af
mcabrol <martin.cabrol@free.fr>
2021-06-29 23:28:48 +0200

    Remove api call

--- profile.js
+++ profile.js
@@ -1,13 +1,6 @@
-function currentTime() {
-	var date = new Date();
-	var hour = date.getHours();
-	var min = date.getMinutes();
-	var sec = date.getSeconds();
-	hour = updateTime(hour);
-	min = updateTime(min);
-	sec = updateTime(sec);
-	document.getElementById("clock").innerText = hour + " : " + min + " : " + sec;
-	var t = setTimeout(function(){ currentTime() }, 1000);
+function currentYear() {
+	var year = new Date().getFullYear();
+	document.getElementById("clock").innerText = `Copyright © ${year} All rights reserved.`;
 }
 
 function updateTime(k) {
@@ -26,25 +19,24 @@ function sendEmail()
 
 $( document ).ready(function() {
 
-	var request = $.ajax({
-		method: "GET",
-		url: "https://mcabrol.github.io/README.md"
-	});
+	// var request = fetch({
+	// 	method: "GET",
+	// 	url: "https://mcabrol.github.io/README.md"
+	// });
 
-	request.done(function(data) {
-		$('#content').append(marked(data));
-	});
+	var data = `## Based in *Paris* and *Brussel* graduated in **graphic design**, student in **computer science**. Currently employed as a **javascript frontend developer**.`;
+	$('#content').append(marked(data));
 
 	$('#key').hover(
-		  function () {
-			  $("#main").css({"color":"transparent"});
-			  $("#contact").css({"color":"white"});
-		  },
-		  function () {
-			  $("#contact").css({"color":"transparent"});
-			  $("#main").css({"color":"white"});
-		  }
-	  );
+		function () {
+			$("#main").css({"color":"transparent"});
+			$("#contact").css({"color":"white"});
+		},
+		function () {
+			$("#contact").css({"color":"transparent"});
+			$("#main").css({"color":"white"});
+		}
+	);
 
-	  currentTime();
+	currentYear();
 });
